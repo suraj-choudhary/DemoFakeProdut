f137a3cb809b8e05d5953feed2f6194c1adf1346
suraj kumar <surajkumar@surajs-MacBook-Air.local>
2023-03-22 07:18:41 +0530

    last

--- FakeProduct/Controller/ProductListVC.swift
+++ FakeProduct/Controller/ProductListVC.swift
@@ -8,25 +8,23 @@
 import UIKit
 /// Produt List Controller
 class ProductListVC: UITableViewController {
-    
     var productViewModel = ProductListViewModel()
     var dummyData: [FakeProductModel]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-    
     override func viewWillAppear(_ animated: Bool) {
-        
         productViewModel.fetchProdut { data in
             self.dummyData = data
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+
+            }
         }
     }
 }
-
-
-
 
 //extension of product list
 extension ProductListVC {
@@ -36,13 +34,15 @@ extension ProductListVC {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        
-        cell.textLabel?.text = dummyData?[0].title
+        if let data = (self.dummyData?[indexPath.row].title) {
+            cell.textLabel?.text = "\(data)"
+        }
+       
+    
+       
         return cell
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
-    
-    
 }
